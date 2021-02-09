37d48fe05d6024dfe892289a01444aa8f94c42c9
jordibaselga <jobaselga@gmail.com>
2021-02-09 18:26:48 +0100

    prueba subir

--- appCoinbot/src/app/app.module.ts
+++ appCoinbot/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { CatalogueComponent } from './components/catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule,
